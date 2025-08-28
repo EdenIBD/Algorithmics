0f9b99797a51dc4ae1bc91cbb05b1a35ee929dc6
EdenIBD <eden.ibadula@yahoo.com>
2025-08-28 21:15:31 +0300

    Added pbinfo547

---
+++ pbinfo547/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >>n;
+    int v[1001];
+
+    for(int i = 1; i<=n; i++){
+        cin >> v[i];
+    }
+
+    int maxx = 0, minn = 1000001;
+
+    for(int i = 1; i<=n;i++){
+        if(v[i] >= maxx) maxx = v[i];
+        if(v[i] <= minn) minn = v[i];
+    }
+
+    int nr = 0;
+
+    for(int i = 1; i<=n;i++){
+        if(v[i] == maxx - minn)nr++;
+    }
+
+    cout <<  nr;
+    return 0;
+}
