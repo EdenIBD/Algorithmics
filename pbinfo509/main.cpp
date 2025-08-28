b6f9009a84694891b5c8275acecc7d19778c70f9
EdenIBD <eden.ibadula@yahoo.com>
2025-08-28 21:15:23 +0300

    Added pbinfo509

---
+++ pbinfo509/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    int v[1001];
+
+    for(int i = 1; i<=n; i++)
+        cin >> v[i];
+
+    for(int i = 1; i<n; i++)
+        for(int j = i+1; j<=n; j++)
+            if(v[i] > v[j]){
+                int aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+            }
+     for(int i = 1; i<=n; i++)
+            cout << v[i] << " ";
+
+    return 0;
+}
