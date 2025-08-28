cf94666d5e473a436f44450a66b3284cb2b0b2cf
EdenIBD <eden.ibadula@yahoo.com>
2025-08-28 21:06:47 +0300

    Added pbinfo30

---
+++ pbinfo30/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void sub(int n, int k){
+    for(int i = n; i>=1; i--){
+        cout << i * k << " ";
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    sub(n, k);
+    return 0;
+}
