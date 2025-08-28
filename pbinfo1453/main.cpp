05ccf85e10511fcc54d42d9dd35b66a4cc72229f
EdenIBD <eden.ibadula@yahoo.com>
2025-08-28 20:39:56 +0300

    Added pbinfo1453

---
+++ pbinfo1453/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    long long v[1001];
+
+    for(int i = 1; i<=n; i++)
+        cin >> v[i];
+
+    for(int p = n; p>=1; p--){
+        if(v[p] % 2 == 0){
+            for(int i = p; i<=n-1; i++)
+                v[i] = v[i+1];
+            n--;
+    }
+    }
+
+    for(int i = 1;i<=n; i++)
+        cout << v[i] << " ";
+
+    return 0;
+}
+
