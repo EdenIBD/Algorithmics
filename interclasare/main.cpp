9cbe82ba239491e8d6e92551db68e7dc066c8455
EdenIBD <eden.ibadula@yahoo.com>
2025-08-31 11:43:18 +0300

    Added interclasare

---
+++ interclasare/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream fin("interclasare.in");
+    ofstream fout("interclasare.out");
+
+    int n, m, p = 1, a[100001], b[100001], c[200002];
+
+    fin >> n;
+    for(int i = 1; i<=n; i++)
+        fin >> a[i];
+
+    fin >> m;
+    for(int j = 1; j<=m; j++)
+        fin >> b[j];
+
+    int i = 1, j = 1;
+
+    while(i <= n && j <= m)
+        if(a[i] < b[j])
+            c[p++] = a[i++];
+        else
+            c[p++] = b[j++];
+
+    while(i <= n)
+        c[p++] = a[i++];
+
+    while(j <= m)
+        c[p++] = b[j++];
+
+    for(int t = 1; t<=p-1; t++)
+        fout << c[t] << " ";
+
+    return 0;
+}
