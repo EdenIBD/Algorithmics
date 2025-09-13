94233e93bf32dd5ab8be29d2fa2082df402cbedb
EdenIBD <eden.ibadula@yahoo.com>
2025-09-13 14:56:03 +0300

    Added pbinfo13

---
+++ pbinfo13/pbinfo13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    char s[11], t[11];
+    cin >> s;
+    strcpy(t, s);
+    for(int i = strlen(s); i >= 1; i--){
+        s[i] = '\0';
+        cout << s << endl; 
+    }
+
+    for(int i = 0; i<strlen(t); i++){
+        cout << t + i << endl;
+    }
+    
+    return 0;
+}
