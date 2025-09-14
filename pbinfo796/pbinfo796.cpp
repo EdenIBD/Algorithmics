9de54675a7f159b603ec9e38ac2d05713041abcf
EdenIBD <eden.ibadula@yahoo.com>
2025-09-14 16:50:11 +0300

    Added pbinfo796

---
+++ pbinfo796/pbinfo796.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int a[51][51];
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int j = 1; j <= m; j++)
+        a[n-1][j] = a[n][j];
+
+        n--;
+    for(int i = 1; i <= n; i++)
+        a[i][m-1] = a[i][m];
+
+        m--;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+}
+    return 0;
+}
