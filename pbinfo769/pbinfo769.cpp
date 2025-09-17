eb10114b9f78e76d4b592030cefc6a13bc211fa8
EdenIBD <eden.ibadula@yahoo.com>
2025-09-17 17:27:04 +0300

    Added pbinfo769

---
+++ pbinfo769/pbinfo769.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int a[101][101];
+
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+            cin >> a[i][j];
+
+    for (int step = 1; step < m; ++step) {
+        for (int j = 1; j < m; ++j) {
+            if (a[1][j] > a[1][j+1]) {
+                for(int i = 1; i <= n; ++i){
+                    int aux = a[i][j];
+                    a[i][j] = a[i][j+1];
+                    a[i][j+1] = aux;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
