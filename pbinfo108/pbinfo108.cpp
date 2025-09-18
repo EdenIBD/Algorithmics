3045aa929872f16a2db8cfc85c7dd85c219acc3d
EdenIBD <eden.ibadula@yahoo.com>
2025-09-18 19:03:04 +0300

    Modified pbinfo108

--- pbinfo108/pbinfo108.cpp
+++ pbinfo108/pbinfo108.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main(){
     long long n, c = 0;
     cin >> n;
-    long long copie = n; 
+    long long copie = n;
+    if(n == 0){
+        cout << 1;
+        return 0;
+    } 
     while(n > 9) n /= 10;
     while(copie){
-        int cif = copie % 10;
-        if(cif == n) c++;
+        long long cif = copie % 10;
+        if(cif == (long long)(n)) c++;
         copie /= 10;
     }
 
