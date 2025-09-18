2df87910cde8ddd63f042f65d6768250213e52f5
EdenIBD <eden.ibadula@yahoo.com>
2025-09-18 18:32:33 +0300

    Added pbinfo4722

---
+++ pbinfo4722/pbinfo4722.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long n, s = 0;
+    cin >> n;
+    while(n>9){ 
+        s += n % 10;
+        n /= 10;
+    }
+
+    if(n == s)cout << "DA";
+    else cout << "NU";
+
+    return 0;
+}
