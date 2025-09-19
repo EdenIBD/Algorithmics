b6ddcaddd7f6259f99ebbfcbe95b28daccd6db04
EdenIBD <eden.ibadula@yahoo.com>
2025-09-20 00:21:55 +0300

    Added pbinfo3246

---
+++ pbinfo3246/pbinfo3246.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool echilibrat(int x){
+    int c = 0, c1 = 0, c2 = 0;
+    while(x){
+        int cif = x % 10;
+        if(cif % 2 == 0)
+            c1++;
+        if(cif % 2 != 0)
+            c2++;
+        c++;
+        x /= 10;
+    }
+
+    if(c1 == c2 && c % 2 == 0) return true;
+    else return false;
+}
+
+int main(){
+    int a, b, minn, maxx, s = 0;
+    cin >> a >> b;
+    if(a > b){
+        minn = b;
+        maxx = a;
+    }
+    else{
+        minn = a;
+        maxx = b;
+    }
+
+    for(int i = minn; i <= maxx; i++)
+        if(echilibrat(i) == true)
+            s += i;
+
+    cout << s;
+    return 0;
+}
